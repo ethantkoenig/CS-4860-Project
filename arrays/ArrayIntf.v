1169391f63b8adc246c3bae401f57f7b8351e93c
Ethan Koenig <ethantkoenig@gmail.com>
2016-12-02 11:32:21 -0500

    Comments for ArrayIntf

--- arrays/ArrayIntf.v
+++ arrays/ArrayIntf.v
@@ -5,9 +5,22 @@ Module Type ArrayInterface (N : VerifiedNaturalInterface).
 
   Parameter M : Type -> Type.
 
+  (* `make n x` creates an array of length n, where each index is
+   * occupied by x. We use nat as the type of n, instead of N.N,
+   * so that we can recurse well-foundedly. *)
   Parameter make : forall A : Type, nat -> A -> M A.
+
+  (* Compute the length of an array *)
   Parameter len : forall A : Type, M A -> N.N.
+
+  (* `get n l` computes the element in index n of array l, or None
+   * if n is not a valid index *)
   Parameter get : forall A : Type, M A -> N.N -> option A.
+
+  (* `set n x l` computes an array identical to l, except that the
+   * n^th index is occupied by x. *)
   Parameter set : forall A : Type, M A -> N.N -> A -> M A.
+
+  (* Compute the concatentation of two arrays *)
   Parameter concat : forall A : Type, M A -> M A -> M A.
 End ArrayInterface.
